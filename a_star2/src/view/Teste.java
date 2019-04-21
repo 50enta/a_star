7a22b87c56f041ebdea1c5f01955242d8d938e36
50enta <valthercinquenta@gmail.com>
2019-04-21 11:31:45 -0800

    alguns teste na consola - parametrizados

--- a_star2/src/view/Teste.java
+++ a_star2/src/view/Teste.java
@@ -1,11 +1,12 @@
 package view;
 
 import estados.Estado;
+import estados.Impressao;
 import estados.Operacao;
 import java.util.ArrayList;
 
 /**
- * 
+ *
  * @author cinquenta
  * @author samira
  * @author lucilia
@@ -13,22 +14,25 @@ import java.util.ArrayList;
 public class Teste {
 
     public static void main(String[] args) {
-
+        ArrayList<Impressao> imp = new ArrayList();
         Operacao op = new Operacao();
-        //Start states
-        ArrayList todosEstados = op.processar(
-                new Estado("Start", 3, 3, false, 1),
-                new Estado("End", 0, 0, true, 0));
-
+        //Iniciar os estados
+        ArrayList<Estado> todosEstados
+                = op.processar(
+                        new Estado("Inicio", 3, 3, false, 1),
+                        new Estado("fim", 0, 0, true, 0)
+                );
+        int numPassos = todosEstados.size()+1;
         if (todosEstados.isEmpty()) {
-            System.out.println("\n\nND SOLUCAO \r\n");
+            System.out.println("\n\n Erro! \r\n");
         } else {
             for (int i = 0; i < todosEstados.size(); i++) {
-                Estado s = (Estado) todosEstados.remove(0);
+                Estado s =  todosEstados.remove(0);
                 System.out.println("Misionarios e Canibais\r\n");
-                System.out.println("Margem esquerda ---------- Margem Direita");
+                System.out.println("Margem esquerda --- Margem Direita");
                 s.imprimir();
                 System.out.println("Total de " + s.getPosicaoEstado() + " Passos!\n");
+//                System.out.println("Total de " + numPassos + " Passos!\n");
             }
         }
     }
