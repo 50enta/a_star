12dcadf1a011ba550144d77bd3853418119fa7e9
50enta <valthercinquenta@gmail.com>
2019-04-21 11:32:46 -0800

    algumas refactoracoes

--- a_star2/src/estados/Operacao.java
+++ a_star2/src/estados/Operacao.java
@@ -3,6 +3,10 @@ package estados;
 import java.util.ArrayList;
 
 /**
+ * Nesta classe, estao os metodos responsaveis por operacoes que nao estao
+ * ligadas directamente a cada estado/no. ou seja, as operacoes que podem
+ * envolver varios estado/nos em simultario. Como por exemplo a busca pela
+ * solucao, envolve todos os estados possiveis dado o estado inicial
  *
  * @author cinquenta
  * @author samira
@@ -11,7 +15,7 @@ import java.util.ArrayList;
 public class Operacao {
 
     private int estadoDoBarcoActual = 0;
-    private ArrayList lista = new ArrayList();
+    private ArrayList<Estado> lista = new ArrayList();
 
     /**
      * Adiciona o estado a lista e faz a linkagem com o no pai
@@ -25,7 +29,7 @@ public class Operacao {
         int direcao = pai.isMargem() ? 1 : -1;
         String novaDesignacao = pai.getDesignacao() + designacao;
         Estado estado = new Estado(novaDesignacao, pai.getNumeroCanibais() + nMiss * direcao,
-                pai.getNumeroMissionarios() + nCan * direcao, !pai.isMargem(), pai, pai.getPosicaoEstado());
+                pai.getNumeroMissionarios() + nCan * direcao, !pai.isMargem(), pai, pai.getPosicaoEstado()+1);
         this.adicionarALista(estado);
     }
 
@@ -41,25 +45,39 @@ public class Operacao {
         }
     }
 
+    /**
+     * Basicamento o metodo principal que por sua vez iterliga os outros metodos
+     * na busca da solucao.
+     *
+     * @param inicio
+     * @param fim
+     * @return
+     */
     public ArrayList processar(Estado inicio, Estado fim) {
         boolean achou = false;
-        ArrayList listinha = new ArrayList();
+        ArrayList auxiliar = new ArrayList();
         adicionarALista(inicio);
         while (getLista().size() > 0 && !achou) {
-            Estado curNode = (Estado) getLista().remove(getEstadoDoBarcoActual());
-            if (curNode.isIguais(fim)) {
-                listinha.add(curNode);
+            Estado estActual = (Estado) getLista().remove(this.getEstadoDoBarcoActual());
+            if (estActual.isIguais(fim)) {
+                auxiliar.add(estActual);
                 achou = true;
             } else {
-                this.gerarNovosEstados(curNode);
+                this.gerarNovosEstados(estActual);
             }
         }
-        return listinha;
+        return auxiliar;
     }
 
+    /**
+     * Os estados sao gerados aleatoriamente e de seguida sao validados, para
+     * verificar se podem fazer parte da lista ou nao.
+     *
+     * @param agora
+     */
     public void gerarNovosEstados(Estado agora) {
         int nCan, nMiss = 0;
-        int nodeName = 1;
+        int designacao = 1;
         for (int i = 0; i <= 2; i++) {
             for (int j = 0; j <= 2; j++) {
                 if (i == 0 && j == 0) {
@@ -70,7 +88,7 @@ public class Operacao {
                 }
                 nMiss = i;
                 nCan = j;
-                addicionarALista("_" + nodeName++, agora, nMiss, nCan);
+                addicionarALista(("_" + designacao++), agora, nMiss, nCan);
             }
         }
     }
